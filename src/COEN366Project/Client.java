a21cf0628a1f7e33d9b6dbe1714602c45abd9a83
Jacob <jacobrodolfo1999@gmail.com>
2021-11-08 18:44:36 -0500

    Added Code for Client

--- src/COEN366Project/Client.java
+++ src/COEN366Project/Client.java
@@ -1,4 +1,32 @@
 package COEN366Project;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 
 public class Client {
+    private Socket socket;
+    private Scanner scanner;
+    private Client(InetAddress serverAddress, int serverPort) throws Exception{
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+    private void start() throws IOException{
+        String input;
+        while(true) {
+            input = scanner.nextLine();
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+            out.println(input);
+            out.flush();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Client client = new Client(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+
+        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+        client.start();
+    }
 }
