92b21acee8a96c15406f90ff62048cd39d1880d2
rami ajam <ajam.rami99@gmail.com>
2021-11-08 18:58:50 -0500

    server and client functions

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		Thread server = new Thread(new Server());
+		Thread client = new Thread(new Client());
+		
+		server.start();
+		client.start();
+		try {
+			server.join();
+			client.join();
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
